2f38e1a3371117c3aeee49bd2578f18fb97ae1d0
Nadav Ben Hur <nadi2022@gmail.com>
2022-10-11 15:06:14 +0300

    delete message added

--- src/index.js
+++ src/index.js
@@ -5,47 +5,67 @@ import Rooms from '../dbRooms.js'
 import Users from '../dbUsers.js'
 import Pusher from 'pusher'
 import cors from 'cors'
-import path from 'path';
-// import {MONGO_DB_USERNAME, MONGO_DB_PASSWORD, PUSHER_KEY, PUSHER_SECRET} from './secrets.js'
+import path from 'path'
+import {
+  MONGO_DB_USERNAME,
+  MONGO_DB_PASSWORD,
+  PUSHER_KEY,
+  PUSHER_SECRET,
+} from '../secrets.js'
 // app config
 const app = express()
 const port = process.env.PORT || 4000
-const __dirname = path.resolve();
+const __dirname = path.resolve()
+const pusher =
+  process.env.NODE_ENV === 'production'
+    ? (pusher = new Pusher({
+        appId: '1486714',
+        key: `${process.env.PUSHER_KEY}`,
+        secret: `${process.env.PUSHER_SECRET}`,
+        cluster: 'eu',
+        useTLS: true,
+      }))
+    : new Pusher({
+        appId: '1486714',
+        key: `${PUSHER_KEY}`,
+        secret: `${PUSHER_SECRET}`,
+        cluster: 'eu',
+        useTLS: true,
+      })
 
-const pusher = new Pusher({
-    appId: "1486714",
-    key: `${process.env.PUSHER_KEY}`,
-    secret: `${process.env.PUSHER_SECRET}`,
-    cluster: "eu",
-    useTLS: true
-  })
-
-  if (process.env.NODE_ENV === 'production') {
-    // Express serve static files on production environment
-    app.use(express.static(path.join(__dirname, 'public')))
+if (process.env.NODE_ENV === 'production') {
+  // Express serve static files on production environment
+  app.use(express.static(path.join(__dirname, 'public')))
 } else {
-    // Configuring CORS
-    const corsOptions = {
-        // Make sure origin contains the url your frontend is running on
-        origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
+  // Configuring CORS
+  const corsOptions = {
+    // Make sure origin contains the url your frontend is running on
+    origin: [
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  }
+  app.use(cors(corsOptions))
 }
-
-
 
 // middleware
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Headers", "*")
-    next()
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  next()
 })
 
 // DB config
 const connection_url =
-process.env.DATABASE_URL || `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.5ki7k6q.mongodb.net/?retryWrites=true&w=majority`
+  process.env.NODE_ENV === 'production'
+    ? `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.5ki7k6q.mongodb.net/?retryWrites=true&w=majority`
+    : `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@cluster0.5ki7k6q.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
@@ -55,78 +75,97 @@ mongoose.connect(connection_url, {
 const db = mongoose.connection
 
 db.once('open', () => {
-    console.log('DB connected')
+  console.log('DB connected')
 
-    const msgCollection = db.collection("messagecontents")
-    const changeStream = msgCollection.watch()
+  const msgCollection = db.collection('messagecontents')
+  const changeStream = msgCollection.watch()
 
-    changeStream.on('change', (change) => {
-        console.log(change)
+  changeStream.on('change', (change) => {
+    console.log(change)
 
-        if (change.operationType === 'insert') {
-            const messageDetails = change.fullDocument
-             pusher.trigger("messages", "inserted", {
-                _id: messageDetails._id,
-                name: messageDetails.name,
-                message: messageDetails.message,
-                timestamp: messageDetails.timestamp,
-                received: messageDetails.received,
-                roomId: messageDetails.roomId
-             })
-        } else {
-            console.log("Error triggering Pusher")
-        }
-    })
+    if (change.operationType === 'insert') {
+      const messageDetails = change.fullDocument
+      pusher.trigger('messages', 'inserted', {
+        _id: messageDetails._id,
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+        roomId: messageDetails.roomId,
+      })
+    } else if(change.operationType === 'delete') {
+        pusher.trigger('messages', 'deleted', change.documentKey._id)
+    }
+     else {
+      console.log('Error triggering Pusher')
+    }
+  })
 })
 
 // api chat routes
 app.get('/', (req, res) => res.status(200).send('hello world'))
 
-
 app.get('/api/messages/sync', (req, res) => {
-    Messages.find((err, data) => {
-        if (err) res.status(500).send(err)
-        else res.status(200).send(data)      
-    })
+  Messages.find((err, data) => {
+    if (err) res.status(500).send(err)
+    else res.status(200).send(data)
+  })
 })
 
 app.post('/api/messages/new', (req, res) => {
-    const dbMessage = req.body
-    Messages.create(dbMessage, (err, data) => {
-        if (err) res.status(500).send(err)
-        else res.status(201).send(data)      
-    })
+  const dbMessage = req.body
+  Messages.create(dbMessage, (err, data) => {
+    if (err) res.status(500).send(err)
+    else res.status(201).send(data)
+  })
+})
+
+app.delete('/api/messages/delete', (req, res) => {
+    const messageId = req.body
+    Messages.findById(messageId, (err, message) => {
+        if (err) { 
+            console.log('DELETE Error: ' + err);
+            res.status(500).send('Error');
+          } else if (message) {
+            message.remove( () => {
+              res.status(200).json(message);
+            });
+         } else {
+            res.status(404).send('Not found');
+          }
+    }
+    )
 })
 
 app.get('/api/rooms', (req, res) => {
-    Rooms.find((err, data) => {
-        if (err) res.status(500).send(err)
-        else res.status(200).send(data)      
-    })
+  Rooms.find((err, data) => {
+    if (err) res.status(500).send(err)
+    else res.status(200).send(data)
+  })
 })
 
 app.post('/api/rooms/new', (req, res) => {
-    const dbRoom = req.body
-    Rooms.create(dbRoom, (err, data) => {
-        if (err) res.status(500).send(err)
-        else res.status(201).send(data)      
-    })
+  const dbRoom = req.body
+  Rooms.create(dbRoom, (err, data) => {
+    if (err) res.status(500).send(err)
+    else res.status(201).send(data)
+  })
 })
 
 // api user routes
 app.get('/api/users', (req, res) => {
-    Users.find((err, data) => {
-        if (err) res.status(500).send(err)
-        else res.status(200).send(data)      
-    })
+  Users.find((err, data) => {
+    if (err) res.status(500).send(err)
+    else res.status(200).send(data)
+  })
 })
 
 app.post('/api/users/new', (req, res) => {
-    const dbUser = req.body
-    Users.create(dbUser, (err, data) => {
-        if (err) res.status(500).send(err)
-        else res.status(201).send(data)      
-    })
+  const dbUser = req.body
+  Users.create(dbUser, (err, data) => {
+    if (err) res.status(500).send(err)
+    else res.status(201).send(data)
+  })
 })
 
 // listen
